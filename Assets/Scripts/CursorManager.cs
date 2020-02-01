85fb38e753b771ff7eb5eae315527b0c8e4a145f
Ludvig Ström <ludvigstrom@gmail.com>
2020-02-01 15:18:50 +0100

    cursor

---
+++ Assets/Scripts/CursorManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorManager : MonoBehaviour
+{
+    public bool hideCursor;
+
+    private void Start()
+    {
+        Cursor.visible = hideCursor;
+    }
+
+    private void Update()
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -35,4 +35,6 @@ public class GameManager : MonoBehaviour
             won = true;
         }        
     }
+
+
 }
